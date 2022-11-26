95297a6826b1b11bd95ce33a24d86cdb63ea7e79
Bocar <59258164+bocarw121@users.noreply.github.com>
2022-11-26 00:49:49 -0500

    Add starter for home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+	isOpen = JSON.parse(localStorage.getItem('isOpen') as string);
+}
