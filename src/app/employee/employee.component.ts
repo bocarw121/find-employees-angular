496700c846a50c6c1f76120d222b4fb7174f6cd7
Bocar <59258164+bocarw121@users.noreply.github.com>
2022-11-26 18:07:58 -0500

    Add Employee

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Employee } from './../../types';
+import { Observable } from 'rxjs';
+import { EmployeesService } from './../employees.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-employee',
-  templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+	selector: 'app-employee',
+	templateUrl: './employee.component.html',
+	styleUrls: ['./employee.component.css'],
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+	employee$: Observable<Employee> | undefined;
+	faLocation = faLocationDot;
 
+	constructor(
+		private route: ActivatedRoute,
+		private heroService: EmployeesService,
+		private location: Location,
+	) {}
+
+	goBack() {
+		this.location.back();
+	}
+
+	ngOnInit() {
+		const id = Number(this.route.snapshot.paramMap.get('id'));
+		this.employee$ = this.heroService.getOneEmployee(id);
+	}
 }
