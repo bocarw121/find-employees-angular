8f38d1f6751c4820f4bbcae8f21b89ef3e2769c8
Bocar <59258164+bocarw121@users.noreply.github.com>
2022-11-27 17:57:51 -0500

    Serve static files from express

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import * as morgan from 'morgan';
 import employeesRouter from './routes/employees.router';
 import * as cors from 'cors';
 import notFound from './middleware/notFound';
+import * as path from 'path';
 
 import errorHandler from './middleware/errorHandler';
 
@@ -12,7 +13,13 @@ app.use(morgan('dev'));
 app.use(cors({ origin: true }));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api', employeesRouter);
+
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use(notFound);
 app.use(errorHandler);
